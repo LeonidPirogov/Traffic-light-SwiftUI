b6c8eda807a543e09ae2648fbb0de5fe8a3945df
LeonidPirogov <lv.pirogof@gmail.com>
2025-10-28 17:01:16 +0300

    Initial Commit

---
+++ Traffic light SwiftUI/Traffic_light_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Traffic_light_SwiftUIApp.swift
+//  Traffic light SwiftUI
+//
+//  Created by Leonid on 28.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Traffic_light_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
